de69f22b67ca6ade254edb7d83eba41447cd4e36
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-03-03 12:05:05 +0700

    Co-authored-by: Kamal Shafi <mastree@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,196 +1,234 @@
 #include <bits/stdc++.h>
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
-
 
 using namespace std;
 
-// For random seeds
-
-
 // Define Constants
 const int NUM_OF_STATION = 3;
-const int MAX_NUM_OF_PASSENGER = 20;
-const int AVG_ARRIVAL_TIME_STATION_1 = 14;
-const int AVG_ARRIVAL_TIME_STATION_2 = 10;
-const int AVG_ARRIVAL_TIME_STATION_3 = 24;
-const double MAX_TIME = 80;
+const int MAX_CAPACITY = 20;
+const int AVG_ARRIVAL_TIME_STATION_1 = 14; // avg per hour
+const int AVG_ARRIVAL_TIME_STATION_2 = 10; // avg per hour
+const int AVG_ARRIVAL_TIME_STATION_3 = 24; // avg per hour
+const double MAX_TIME = 80 * 60.0; // minutes
+const double BUS_SPEED = 30.0 / 60.0; // 30 miles per hour -->> speed per minutes
 
-
-Station station[NUM_OF_STATION + 1];
+// For Random Functions
+mt19937 rng32(chrono::steady_clock::now().time_since_epoch().count());
+unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+default_random_engine generator(seed);
+uniform_real_distribution<> dis(0, 1);
 
 int get_uniform_int_distribution(int low, int high) {
-    default_random_engine generator;
     uniform_int_distribution<int> distribution(low, high);
     return distribution(generator);
 }
 
-mt19937 rng32(chrono::steady_clock::now().time_since_epoch().count());
-uniform_real_distribution<> dis(0, 1);
-
 double expon(double mean) {
-    return min(-mean * log(dis(rng32)), MAX_TIME);
+    return min(-mean * log(dis(rng32)), 80.0);
 }
 
-double simclock = 0.0; // current time in simulation
+// Simulation Clock
+double simclock = 0.0; // current time in simulation (minutes)
 
 // Define Data Structures with structs
 struct Person {
-    double arrival_time; // di generate pake expon
+    double arrival_time; // generated with expon
+    double departure_time = MAX_TIME;
+
+    double bus_in_time;
+    double bus_out_time = MAX_TIME;
+
     int arrival_location;
-    int destination; // (kalau arrival_location = 3, generate destination = 1 atau 2 pake probability)
+    int destination; // (if arrival_location = 3, generate destination = 1 or 2 with probability)
     
     Person() : arrival_time(0.0), arrival_location(0), destination(0) {}
     Person(double arrTime, int loc, int dest) : arrival_time(arrTime), arrival_location(loc), destination(dest) {}
 };
 
-struct Bus {
-    deque<person> passengers[3];
-    int num_of_passenger;
-    int place; // where the bus is at the moment (ex. at terminal 3)
-    double min_time_at_place[3];
-    double max_time_at_place[3];
-    double avg_time_at_place[3];
-    double num_at_place[3];
-    
-    double unload() {
-        double timer = 0.0;
-        while (timer < 5.0) {
-            if (passengers[place].empty()) break;
-            int unload_time = get_uniform_int_distribution(16, 24);
-            simclock += unload_time;
-            passengers[place].pop_front();
-            timer += unload_time;
-            num_of_passenger--;
-        }
-        return timer;
-    }
-
-    double load(double timer) {
-        while (num_of_passenger < MAX_NUM_OF_PASSENGER) {
-            if (timer >= 5.0) break;
-            if (station[place].empty()) break;
-            Person currentPerson = station[place].front();
-            double delay = simclock - currentPerson.arrival_time;
-            total_delay[place] += delay;
-            max_delay[place] = max(max_delay[place], delay);
-
-            int load_time = get_uniform_int_distribution(15, 25);
-            simclock += load_time;
-            timer += load_time;
-            passengers.pop_front();
-
-            num_of_passenger++;
-        }
-        return timer;
-    }
-
-    void stop() {
-        double timer = unload();
-        if (timer < 5.0) {
-            timer = load(timer);
-        }
-        timer = max(5.0, timer);
-        min_time_at_place[place] = min(min_time_at_place[place], timer);
-        max_time_at_place[place] = max(min_time_at_place[place], timer);
-        avg_time_at_place[place] = (avg_time_at_place[place] * num_at_place[place] + timer) / (num_at_place[place] + 1);
-        num_at_place[place]++;
-    }
-};
-
 struct Station {
+    int id;
     deque<Person> queue_line;
-};
+    deque<Person> departed;
+
+    Person depart(double time_of_departure) {
+        assert(!queue_line.empty());
+        Person temp = queue_line.front();
+        queue_line.pop_front();
+        temp.departure_time = time_of_departure;
+        departed.push_back(temp);
+
+        return temp;
+    }
+} station[NUM_OF_STATION + 1];
+
+struct Bus {
+    deque<Person> passengers[NUM_OF_STATION + 1];
+    deque<Person> arrived;
+    int place; // where the bus is at the moment (ex. at terminal 3)
+    double min_time_at_station[NUM_OF_STATION + 1];
+    double max_time_at_station[NUM_OF_STATION + 1];
+    double avg_time_at_station[NUM_OF_STATION + 1];
+    double num_at_station[NUM_OF_STATION + 1];
+
+    Bus() {
+        for (int i = 1; i <= NUM_OF_STATION; i++) {
+            min_time_at_station[i] = INT_MAX;
+            max_time_at_station[i] = num_at_station[i] = avg_time_at_station[i] = 0;
+        }
+    }
+
+    int get_num_of_passengers() {
+        int ret = 0;
+        for (int i = 1; i <= NUM_OF_STATION; i++) {
+            ret += passengers[i].size();
+        }
+        return ret;
+    }
+
+    void unload(Person& temp, double arrived_time){
+        temp.bus_out_time = arrived_time;
+        arrived.push_back(temp);
+    }
+    
+    void transit(Station& st) {
+        double initialclock = simclock;
+        double mxclock = simclock + 5.0;
+        int st_num = st.id;
+        
+        // unload
+        while (!passengers[st_num].empty() && simclock < MAX_TIME) {
+            double unload_time = ((double) get_uniform_int_distribution(16, 24)) / 60.0;
+            unload(passengers[st_num].front(), simclock);
+            passengers[st_num].pop_front();
+
+            simclock += unload_time;
+            mxclock = max(mxclock, simclock);
+        }
+        // load
+        while (get_num_of_passengers() < MAX_CAPACITY && !st.queue_line.empty() && st.queue_line.front().arrival_time <= mxclock && simclock < MAX_TIME) {
+            double load_time = ((double) get_uniform_int_distribution(15, 25)) / 60.0;
+            if (st_num == 1) {
+                cerr << "-----\n";
+                cerr << "[1] " << simclock << '\n';
+                cerr << "-----\n";
+            }
+            Person temp = st.depart(simclock);
+            simclock += load_time;
+            mxclock = max(mxclock, simclock);
+
+            passengers[temp.destination].push_back(temp);
+        }
+
+        simclock = mxclock;
+
+        double time_in_station = simclock - initialclock;
+        min_time_at_station[st_num] = min(min_time_at_station[st_num], time_in_station);
+        max_time_at_station[st_num] = max(min_time_at_station[st_num], time_in_station);
+        avg_time_at_station[st_num] = (avg_time_at_station[st_num] * num_at_station[st_num] + time_in_station) / (num_at_station[st_num] + 1);
+        num_at_station[st_num]++;
+    }
+} bus;
 
 void initialize() {
     // create queue_line of station 1
+    station[1].id = 1;
+    station[2].id = 2;
+    station[3].id = 3;
     {
         double last_time = 0.0;
         while (1) {
-            double inter = expon(AVG_ARRIVAL_TIME_STATION_1);
+            double inter = expon(AVG_ARRIVAL_TIME_STATION_1) * 60.0;
+            cerr << "[I] " << inter << '\n';
             if (last_time + inter > MAX_TIME) break;
             last_time += inter;
             int loc = 1;
             int dest = 3;
 
-            station[1].push_back(Person(last_time, loc, dest));
+            station[1].queue_line.push_back(Person(last_time, loc, dest));
         }
     }
     // create queue_line of station 2
     {
         double last_time = 0.0;
         while (1) {
-            double inter = expon(AVG_ARRIVAL_TIME_STATION_2);
+            double inter = expon(AVG_ARRIVAL_TIME_STATION_2) * 60.0;
             if (last_time + inter > MAX_TIME) break;
             last_time += inter;
             int loc = 2;
             int dest = 3;
 
-            station[2].push_back(Person(last_time, loc, dest));
+            station[2].queue_line.push_back(Person(last_time, loc, dest));
         }
     }
     // create queue_line of station 3
     {
         double last_time = 0.0;
         while (1) {
-            double inter = expon(AVG_ARRIVAL_TIME_STATION_3);
+            double inter = expon(AVG_ARRIVAL_TIME_STATION_3) * 60.0;
             if (last_time + inter > MAX_TIME) break;
             last_time += inter;
             int loc = 3;
             int dest;
             {
                 int temp = get_uniform_int_distribution(1, 1000);
+                cerr << "[T] " << temp << '\n';
                 if (temp <= 583) { // 0.583 chance to go to station 1
                     dest = 1;
                 } else {
                     dest = 2;
                 }
             }
-            station[3].push_back(Person(last_time, loc, dest));
+            station[3].queue_line.push_back(Person(last_time, loc, dest));
         }
+    }
+    for (int i = 1; i <= NUM_OF_STATION; i++) {
+        cerr << "-----\n";
+        cerr << "[S] " << i << " => " << station[i].queue_line.size() << '\n';
+        for (Person& p : station[i].queue_line) {
+            cerr << "[P] " << p.destination << '\n';
+        }
+        cerr << "-----\n";
     }
 }
 
-// void initialize() {
-//     simclock = 0.0;
-    
-//     num_q_in_terminal_1 = 0;
-//     num_q_in_terminal_2 = 0;
-//     num_q_in_terminal_3 = 0;
-//     time_last_event = 0.0;
-
-//     total_delay
-
-//     num_custs_delayed = 0;
-//     total_of_delays = 0.0;
-//     area_num_in_q = 0.0;
-//     area_server_status = 0.0;
-    
-//     time_next_event[1] = simclock + expon(mean_interarrival);
-//     time_next_event[2] = INT_MAX;
-// }
-
-// void timing() {
-//     int i;
-//     float min_time_next_event = INT_MAX;
-//     next_event_type = 0;
-//     /* Determine the event type of the next event to occur */
-//     for (i=1; i<= num_events; i++) {
-//         if (time_next_event[i] < min_time_next_event) {
-//             min_time_next_event = time_next_event[i];
-//             next_event_type = i;
-//         }
-//     }
-//     if (next_event_type == 0) {
-//     /* The event list is empty so stop the simulation */
-//         exit(1);
-//     }
-//     simclock = min_time_next_event;
-// }
-
 int main() {
+    initialize();
+    while (simclock < MAX_TIME) {
+        // depart from station 3
+        // distance 3 - 1 = 4.5 miles
+        {
+            double need = 4.5 / BUS_SPEED;
+            simclock += need;
+        }
+        if (simclock >= MAX_TIME) break;
+        // arrived at station 1
+        bus.transit(station[1]);
+        if (simclock >= MAX_TIME) break;
 
+        // depart from station 1
+        // distance 1 - 2 = 1 miles
+        {
+            double need = 1.0 / BUS_SPEED;
+            simclock += need;
+        }
+        if (simclock >= MAX_TIME) break;
+
+        // arrived at station 2
+        bus.transit(station[2]);
+        if (simclock >= MAX_TIME) break;
+
+        // depart from station 2
+        // distance 2 - 3 = 4.5 miles
+        {
+            double need = 4.5 / BUS_SPEED;
+            simclock += need;
+        }
+        if (simclock >= MAX_TIME) break;
+        //arrived at station 3
+        bus.transit(station[3]);
+        if (simclock >= MAX_TIME) break;
+        cerr << "[+] " << simclock << '\n';
+    }
+    cerr << "FINISHED";
 
     return 0;    
 }
